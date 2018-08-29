690eb1ed201f3c4b155b955135b1bcee4942b8c8
lzq2016 <357881371@qq.com>
2018-08-30 00:18:24 +0800

    lzq

--- src/pages/topic/topicBrief/topicBrief.ts
+++ src/pages/topic/topicBrief/topicBrief.ts
@@ -1,8 +1,8 @@
-import {Component,OnInit,OnDestroy} from '@angular/core'
+import { Component, OnInit, OnDestroy } from '@angular/core'
 import { HttpClient } from '../../../providers/httpClient';
-import {NavParams,ModalController} from 'ionic-angular';
+import { NavParams, ModalController } from 'ionic-angular';
 import { ServiceConfig } from '../../../providers/service.config';
-import {ActiveUserPage} from '../../activeuser/activeuser'
+import { ActiveUserPage } from '../../activeuser/activeuser'
 
 @Component({
   selector: 'page-topicBrief',
@@ -16,22 +16,27 @@ export class TopicBriefPage implements OnInit, OnDestroy {
 
   constructor(
     public http: HttpClient,
-    public navParams:NavParams,
+    public navParams: NavParams,
     public modalCtrl: ModalController) {
   }
 
-  ngOnInit(){
-   console.log("init")
-   this.topicId = this.navParams.data.id
-   this.initloaded();
-  }   
+  ngOnInit() {
+    console.log("init")
+    this.topicId = this.navParams.data.id
+    this.initloaded();
+  }
 
-  initloaded(){
+  initloaded() {
     let self = this;
-      self.http.get(ServiceConfig.TOPICLIST + self.topicId, function (data) {
-        console.log(data);
-        self.topicInfo = data;
+    self.http.get(ServiceConfig.TOPICLIST + self.topicId, function (data) {
+      console.log(data);
+      self.topicInfo = data;
     });
+    self.http.get("api/v1/topic/" + self.topicId + "/hot_users/", function (data) {
+      console.log(data);
+      
+    });
+
   }
 
   // doInfinite(infiniteScroll) {
@@ -43,16 +48,16 @@ export class TopicBriefPage implements OnInit, OnDestroy {
   //   }
   // }
 
-  goMore(){
-    let profileModal = this.modalCtrl.create(ActiveUserPage, { id:this.topicId},{showBackdrop:true,enableBackdropDismiss:true});
+  goMore() {
+    let profileModal = this.modalCtrl.create(ActiveUserPage, { id: this.topicId }, { showBackdrop: true, enableBackdropDismiss: true });
     profileModal.onDidDismiss(data => {
       console.log(data);
     });
     profileModal.present();
   }
 
-  ngOnDestroy(){ 
+  ngOnDestroy() {
     console.log("destroy")
   }
-  
+
 }
